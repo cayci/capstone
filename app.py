e4ed0f6c63fc1f4239e3621c82ddaae21878277f
cayci <cayci724@gmail.com>
2023-01-06 23:00:15 -0500

    got actor working

--- app.py
+++ app.py
@@ -1,30 +1,140 @@
 import os
-from flask import Flask
-from models import setup_db
+import traceback
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, Actor, ActorsInMovies, Movie, db
 from flask_cors import CORS
-from .auth.auth import AuthError, requires_auth
+from auth import AuthError, requires_auth
+import json
+from pprint import pprint
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+        
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': 
-            greeting = greeting + "!!!!! You are doing great in this Udacity project."
-        return greeting
+    @app.route('/actors', methods=['GET'])
+    #@requires_auth('get:actors')
+    def get_actors():
+        actors=Actor.query.order_by(Actor.name).all()
+        formatted_actors=[]
+        for actor in actors:
+            formatted_actors.append(actor.fields_dict())
+        return jsonify({
+            "success":True,
+            "actors": formatted_actors
+        })
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    @app.route('/actors', methods=['POST'])
+    #@requires_auth('post:actors')
+    def create_actor():
+        body = request.get_json()
+        name = body.get('name')
+        age = body.get('age')
+        gender = body.get('gender')
+        new_actor = Actor(name=name, age=age, gender=gender)
+        try:
+            new_actor.insert()
+            return jsonify({
+                "success": True,
+                "actor": new_actor.fields_dict()
+            })
+        except Exception as err:
+            print(traceback.format_exc())
+            abort(422)
+
+
+    @app.route('/actors/<actor_id>', methods=['PATCH'])
+    #@requires_auth('patch:actors')
+    def update_actor(actor_id):
+        try:
+            actor_for_update = Actor.query.filter_by(id=actor_id).all()
+            if len(actor_for_update) == 1:
+                body=request.get_json()
+                name = body.get("name")
+                age = body.get("age")
+                gender = body.get("gender")
+                if name is not None: 
+                    actor_for_update[0].name = name
+                if age is not None:
+                    actor_for_update[0].age = age
+                if gender is not None:
+                    actor_for_update[0].gender = gender
+                actor_for_update[0].update()
+                return jsonify({
+                    "success":True,
+                    "updated":actor_for_update[0].fields_dict()
+                })
+            else:
+                abort(404)
+        except Exception as err:
+            print(traceback.format_exc())
+            abort(422)
+
+
+    @app.route('/actors/<actor_id>', methods=['DELETE'])
+    #@requires_auth('delete:actirs')
+    def delete_actor(actor_id):
+        try:
+            actor_for_deletion = Actor.query.filter_by(id=actor_id).all()
+            pprint(actor_for_deletion)
+            if len(actor_for_deletion) == 1:
+                actor_for_deletion[0].delete()
+                return jsonify({
+                    "success":True,
+                    "delete": actor_id
+                })
+            else:
+                abort(404)
+        except:
+            print(traceback.format_exc())
+            abort(404)
+
 
     return app
 
 app = create_app()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('errors/500.html'), 500
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+
+'''
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -9,10 +9,6 @@ if database_path.startswith("postgres://"):
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,9 +17,6 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
-'''
-Person
-Have title and release year
 '''
 class Person(db.Model):  
   __tablename__ = 'People'
@@ -41,3 +34,148 @@ class Person(db.Model):
       'id': self.id,
       'name': self.name,
       'catchphrase': self.catchphrase}
+'''
+
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(1))
+    movies = db.relationship('ActorsInMovies', backref='actor', lazy=True)
+
+    def fields_dict(self):
+      return {
+          'id': self.id,
+          'name': self.name,
+          'age': self.age,
+          'gender': self.gender
+      }
+    
+    def insert(self):
+        try:
+          db.session.add(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise
+    
+    def update(self):
+        try:
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise
+    
+    def delete(self):
+        try:
+          db.session.delete(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise
+
+    def __repr__(self):
+      return'<Actor {}>'.format(self.name)
+
+
+    # TODO: implement missing fields as a database migration using Flask-Migrate
+
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime)
+    actors = db.relationship('ActorsInMovies', backref='movie', lazy=True)
+
+    def fields_dict(self):
+      return {
+          'id': self.id,
+          'title': self.title,
+          'release_date': self.release_date,
+      }
+    
+    def insert(self):
+        try:
+          db.session.add(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+    
+    def update(self):
+        try:
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+    
+    def delete(self):
+        try:
+          db.session.delete(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+   
+    def __repr__(self):
+      return'<Movie {}>'.format(self.name)
+
+
+class ActorsInMovies(db.Model):
+    __tablename__ = 'ActorsInMovies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    actor_id = db.Column(db.Integer, db.ForeignKey('Actor.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('Movie.id'), nullable=False)
+
+    def fields_dict(self):
+      return {
+          'id': self.id,
+          'actor_id': self.actor_id,
+          'movie_id': self.movie_id,
+      }
+    
+    def insert(self):
+        try:
+          db.session.add(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+    
+    def update(self):
+        try:
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+    
+    def delete(self):
+        try:
+          db.session.delete(self)
+          db.session.commit()
+        except:
+          db.session.rollback()
+          raise 
+        finally:
+          db.session.close()
+    
+    def __repr__(self):
+      return'<Show {}>'.format(self.name)
+
+
